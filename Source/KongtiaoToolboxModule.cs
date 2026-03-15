a3493298174f684d42133c48a7deffd98998563d
Kongtiao <kongtiao92@gmail.com>
2026-03-15 20:27:03 +0800

    Initial commit

---
+++ Source/KongtiaoToolboxModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Celeste.Mod.KongtiaoToolbox;
+
+namespace Celeste.Mod.KongtiaoToolbox;
+
+public class KongtiaoToolboxModModule : EverestModule {
+    public static KongtiaoToolboxModModule Instance { get; private set; }
+
+    public override Type SettingsType => typeof(KongtiaoToolboxModModuleSettings);
+    public static KongtiaoToolboxModModuleSettings Settings => (KongtiaoToolboxModModuleSettings) Instance._Settings;
+
+    public override Type SessionType => typeof(KongtiaoToolboxModuleSession);
+    public static KongtiaoToolboxModuleSession Session => (KongtiaoToolboxModuleSession) Instance._Session;
+
+    public override Type SaveDataType => typeof(KongtiaoToolboxModuleSaveData);
+    public static KongtiaoToolboxModuleSaveData SaveData => (KongtiaoToolboxModuleSaveData) Instance._SaveData;
+
+    public KongtiaoToolboxModModule() {
+        Instance = this;
+#if DEBUG
+        // debug builds use verbose logging
+        Logger.SetLogLevel(nameof(KongtiaoToolboxModModule), LogLevel.Verbose);
+#else
+        // release builds use info logging to reduce spam in log files
+        Logger.SetLogLevel(nameof(KongtiaoToolboxModModule), LogLevel.Info);
+#endif
+    }
+
+    public override void Load() {
+        
+    }
+
+    public override void Unload() {
+        
+    }
+}
