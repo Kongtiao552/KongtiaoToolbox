6235b8f4210164e31e650e6a643ed734f59231a7
Kongtiao <kongtiao92@gmail.com>
2026-03-16 19:43:44 +0800

    -

--- Source/Extensions.cs
+++ Source/Extensions.cs
@@ -1,6 +1,8 @@
+using Celeste.Mod;
+
 namespace KongtiaoToolbox.Extensions
 {
-    public static class ExtensionMethods
+    public static class Extensions
     {
         public static void Log(this string message, LogLevel level = LogLevel.Debug) => Logger.Log(level, "KongtiaoToolbox", message);
     }
